e265d0bb5292cd00956974a82dc2b4d23e73c589
Ryan Fernanda <84463811+ryanfernanda@users.noreply.github.com>
2024-03-10 22:36:55 +0700

    Loops.py

---
+++ Introduction/Loops.py
@@ -0,0 +1,12 @@
+'''
+QUESTION:
+The provided code stub reads and integer, n, from STDIN. For all non-negative integers i<n, print i^2.
+
+ANSWER :
+-- using Python 3
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
