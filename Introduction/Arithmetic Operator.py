10f54a6727a4b31f830bd259d77d164c2e96ea67
Ryan Fernanda <84463811+ryanfernanda@users.noreply.github.com>
2024-03-10 21:33:29 +0700

    Arithmetic Operator.py

---
+++ Introduction/Arithmetic Operator.py
@@ -0,0 +1,18 @@
+'''
+QUESTION:
+The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+- The first line contains the sum of the two numbers.
+- The second line contains the difference of the two numbers (first - second).
+- The third line contains the product of the two numbers.
+
+ANSWER :
+-- using Python 3
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a+b)
+    print(a-b)
+    print(a*b)
